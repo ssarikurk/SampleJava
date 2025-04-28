f8598b57d2d9cccd6e2dff86f7d13e8728fdee00
Suleyman Sarikurk <suleymansarikurk@gmail.com>
2025-04-28 21:46:09 +0300

    mysql sample added4

--- src/test/java/org/cucumber/step_definitions/Mysql_Defs.java
+++ src/test/java/org/cucumber/step_definitions/Mysql_Defs.java
@@ -4,13 +4,20 @@ import io.cucumber.java.en.Given;
 import org.cucumber.utilities.ConfigurationReader;
 import org.cucumber.utilities.SqlUtils;
 
+import java.util.List;
+import java.util.Map;
+
 public class Mysql_Defs {
     @Given("Get data from {string} db")
     public void getDataFromDb(String mysqlDbName) {
 
         SqlUtils.createConnection(ConfigurationReader.get("MySQLIp"), mysqlDbName, "root", "retrace123");
 
+        List<Map<String, Object>> patientData = SqlUtils.getQueryResultMap("select *  from patient p where PatNum = '1621';");
+        System.out.println("patientData = " + patientData);
 
+        String patientName = patientData.get(0).get("FName").toString();
+        System.out.println("patientName = " + patientName);
 
 
     }
